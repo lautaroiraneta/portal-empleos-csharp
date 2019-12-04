0943bc22498f76b6778aa2c549569f95520a85cb
Lautaro Irañeta <Lautaro.Iraneta@thomsonreuters.com>
2019-12-04 03:32:30 -0300

    Aprobacion de Usuarios

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using PortalEmpleos.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PortalEmpleos.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class UsuarioController : ControllerBase
+	{
+		private readonly IConfiguration _configuration;
+
+		public UsuarioController(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+
+		[HttpGet]
+		public List<Usuario> GetList()
+		{
+			var usuarios = new List<Usuario>();
+
+			string connectionstring = _configuration.GetConnectionString("DefaultConnectionString");
+			SqlConnection connection = new SqlConnection(connectionstring);
+			connection.Open();
+
+			SqlCommand com = new SqlCommand("select id, nombre_usuario, pass_usuario, tipo_usuario, alumno, empresa, alta, aprobado, nombre, apellido from usuarios", connection);
+			SqlDataReader dr = com.ExecuteReader();
+
+			while (dr.Read())
+			{
+				var usuario = new Usuario();
+				usuario.Id = dr["id"].ToString();
+				usuario.NombreUsuario = dr["nombre_usuario"].ToString();
+				usuario.Password = dr["pass_usuario"].ToString();
+				usuario.TipoUsuario = dr["tipo_usuario"].ToString();
+				usuario.AlumnoId = dr["alumno"].ToString();
+				usuario.EmpresaId = dr["empresa"].ToString();
+				usuario.Alta = Convert.ToDateTime(dr["alta"]);
+				usuario.Aprobado = dr["aprobado"] == DBNull.Value ? false : Convert.ToBoolean(dr["aprobado"]);
+				usuario.Nombre = dr["nombre"].ToString();
+				usuario.Apellido = dr["apellido"].ToString();
+
+				usuarios.Add(usuario);
+			}
+
+			connection.Close();
+
+			return usuarios;
+		}
+
+		[HttpPost]
+		[Route("set-aprobado")]
+		public void SetAprobado([FromBody] Usuario u)
+		{
+			string connectionstring = _configuration.GetConnectionString("DefaultConnectionString");
+			SqlConnection connection = new SqlConnection(connectionstring);
+			connection.Open();
+
+			SqlCommand com = new SqlCommand("update usuarios set aprobado = @aprobado where id = @id", connection);
+			com.Parameters.AddWithValue("@aprobado", u.Aprobado);
+			com.Parameters.AddWithValue("@id", u.Id);
+
+			com.ExecuteReader();
+
+			connection.Close();
+		}
+	}
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEmpleos.Models
+{
+	public class Usuario
+	{
+		public string Id { get; set; }
+		public string NombreUsuario { get; set; }
+		public string Password { get; set; }
+		public string TipoUsuario { get; set; }
+		public string AlumnoId { get; set; }
+		public string EmpresaId { get; set; }
+		public DateTime Alta { get; set; }
+		public bool Aprobado { get; set; }
+		public string Nombre { get; set; }
+		public string Apellido { get; set; }
+	}
+}
